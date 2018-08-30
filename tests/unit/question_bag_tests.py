9838e8c261d81d7b191d2743d72be712276ff642
Thomas Busby <tom@busby.ninja>
2018-08-30 22:03:02 +0200

    Set up VSCode environment

--- tests/unit/question_bag_tests.py
+++ tests/unit/question_bag_tests.py
@@ -10,14 +10,17 @@ from polish_case_trainer.word.word_bag import WordBag
 def test_cannot_instantiate_question_bag_abstract_object():
     question_bag = QuestionBag()
 
+
 @raises(TypeError)
 def test_noun_case_question_bag_instantiation_fails_without_arguments():
     question_bag = NounCaseQuestionBag()
+
 
 @raises(TypeError)
 def test_noun_case_question_bag_instantiation_fails_if_first_argument_isnt_word_bag_object():
     noun_bag = Mock()
     question_bag = NounCaseQuestionBag(noun_bag, None, None, None)
+
 
 @raises(TypeError)
 def test_noun_case_question_bag_instantiation_fails_if_second_argument_isnt_adjective_object():
@@ -25,12 +28,15 @@ def test_noun_case_question_bag_instantiation_fails_if_second_argument_isnt_adje
     adjective_bag = Mock()
     question_bag = NounCaseQuestionBag(noun_bag, adjective_bag, None, None)
 
+
 @raises(TypeError)
 def test_noun_case_question_bag_instantiation_fails_if_third_argument_isnt_list():
     noun_bag = Mock(spec=WordBag)
     adjective_bag = Mock(spec=WordBag)
     allowed_numbers = "invalid"
-    question_bag = NounCaseQuestionBag(noun_bag, adjective_bag, allowed_numbers, None)
+    question_bag = NounCaseQuestionBag(
+        noun_bag, adjective_bag, allowed_numbers, None)
+
 
 @raises(TypeError)
 def test_noun_case_question_bag_instantiation_fails_if_fourth_argument_isnt_list():
@@ -38,15 +44,19 @@ def test_noun_case_question_bag_instantiation_fails_if_fourth_argument_isnt_list
     adjective_bag = Mock(spec=WordBag)
     allowed_numbers = []
     allowed_cases = "invalid"
-    question_bag = NounCaseQuestionBag(noun_bag, adjective_bag, allowed_numbers, allowed_cases)
+    question_bag = NounCaseQuestionBag(
+        noun_bag, adjective_bag, allowed_numbers, allowed_cases)
+
 
 def test_noun_case_question_bag_instantiation_succeeds_with_valid_arguments():
     noun_bag = Mock(spec=WordBag)
     adjective_bag = Mock(spec=WordBag)
     allowed_numbers = []
     allowed_cases = []
-    question_bag = NounCaseQuestionBag(noun_bag, adjective_bag, allowed_numbers, allowed_cases)
-    assert isinstance(question_bag, QuestionBag) and isinstance(question_bag, NounCaseQuestionBag)
+    question_bag = NounCaseQuestionBag(
+        noun_bag, adjective_bag, allowed_numbers, allowed_cases)
+    assert isinstance(question_bag, QuestionBag) and isinstance(
+        question_bag, NounCaseQuestionBag)
 
 # Some more tests here would be good, after the coupling between NounCaseQuestionBag
 # and NounCaseQuestion is removed.
--- tests/unit/question_tests.py
+++ tests/unit/question_tests.py
@@ -14,6 +14,7 @@ def _get_mock_noun(basic_form, gender, case_supported=True):
     noun.supports = Mock(return_value=case_supported)
     return noun
 
+
 def _get_mock_adjective(basic_form, gender_supported=True, case_supported=True):
     adjective = Mock(spec=Adjective)
     adjective.get_basic_form = Mock(return_value=basic_form)
@@ -23,24 +24,29 @@ def _get_mock_adjective(basic_form, gender_supported=True, case_supported=True):
         adjective.set_gender.side_effect = GenderNotSupported("gender")
     return adjective
 
+
 @raises(TypeError)
 def test_cannot_instantiate_question_abstract_object():
     question = Question()
 
+
 @raises(TypeError)
 def test_noun_case_question_instantiation_fails_without_arguments():
     question = NounCaseQuestion()
+
 
 @raises(TypeError)
 def test_noun_case_question_instantiation_fails_if_first_argument_isnt_word_object():
     noun = Mock()
     question = NounCaseQuestion(noun, None, None, None)
 
+
 @raises(TypeError)
 def test_noun_case_question_instantiation_fails_if_second_argument_isnt_adjective_object():
     noun = Mock(spec=Word)
     adjective = Mock()
     question = NounCaseQuestion(noun, adjective, None, None)
+
 
 @raises(GenderNotSupported)
 def test_noun_case_question_fails_if_adjective_doesnt_have_forms_for_gender():
@@ -49,12 +55,14 @@ def test_noun_case_question_fails_if_adjective_doesnt_have_forms_for_gender():
     question = NounCaseQuestion(noun, adjective, "plural", "nominative")
     adjective.set_gender.assert_called()
 
+
 @raises(CaseNotSupported)
 def test_noun_case_question_fails_if_noun_doesnt_support_case_form():
     noun = _get_mock_noun("kobieta", "f", False)
     adjective = _get_mock_adjective("dobry")
     question = NounCaseQuestion(noun, adjective, "plural", "nominative")
     noun.supports.assert_called()
+
 
 @raises(CaseNotSupported)
 def test_noun_case_question_fails_if_nominative_singular_is_requested():
@@ -63,6 +71,7 @@ def test_noun_case_question_fails_if_nominative_singular_is_requested():
     question = NounCaseQuestion(noun, adjective, "singular", "nominative")
     adjective.supports.assert_called()
 
+
 @raises(CaseNotSupported)
 def test_noun_case_question_fails_if_adjective_doesnt_support_case_form():
     noun = _get_mock_noun("kobieta", "f")
@@ -70,11 +79,14 @@ def test_noun_case_question_fails_if_adjective_doesnt_support_case_form():
     question = NounCaseQuestion(noun, adjective, "plural", "nominative")
     adjective.supports.assert_called()
 
+
 def test_noun_case_question_instantiation_succeeds_if_passed_all_valid_arguments():
     noun = _get_mock_noun("kobieta", "f")
     adjective = _get_mock_adjective("dobry")
     question = NounCaseQuestion(noun, adjective, "plural", "nominative")
-    assert isinstance(question, Question) and isinstance(question, NounCaseQuestion)
+    assert isinstance(question, Question) and isinstance(
+        question, NounCaseQuestion)
+
 
 def test_noun_case_question_get_question_text():
     noun = _get_mock_noun("kobieta", "f")
@@ -88,6 +100,7 @@ def test_noun_case_question_get_question_text():
     assert question.get_question_text() == u"Noun: chłopiec (m pers)\nAdjective: mały\n" +\
         "Decline for singular accusative\n"
 
+
 def test_incorrect_answer_evaluates_false():
     noun = _get_mock_noun("kobieta", "f")
     noun.get_case_form = Mock(return_value=u"kobietę")
@@ -95,6 +108,7 @@ def test_incorrect_answer_evaluates_false():
     adjective.get_case_form = Mock(return_value=u"dobrą")
     question = NounCaseQuestion(noun, adjective, "singular", "accusative")
     assert not question.evaluate(u"incorrect answer")
+
 
 def test_correct_answer_evaluates_true():
     noun = _get_mock_noun("kobieta", "f")
